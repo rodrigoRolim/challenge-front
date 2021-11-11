9c0cbac5329740bec5b409c59a01ace9d13b6afa
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-11 14:41:44 -0300

    feat(component): create base card component

---
+++ src/components/ACard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Acard">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Acard"
+}
+</script>
+
+<style lang="scss" scoped>
+.Acard {
+  background-color: #faf9f9;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 1rem;
+}
+</style>
