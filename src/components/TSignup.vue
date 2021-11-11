689f6327cbcb7e276226d9f5cdfbb9048d335b7a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-11 14:42:53 -0300

    feat(component): create template for signup by users

---
+++ src/components/TSignup.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="TSignup">
+    <m-signup-form />
+  </div>
+</template>
+
+<script>
+import MSignupForm from "./MSignupForm";
+
+export default {
+  components: { MSignupForm },
+  name: "TSignup"
+}
+</script>
+
+<style>
+.TSignup {
+  display: flex;
+  justify-content: center;
+}
+</style>
