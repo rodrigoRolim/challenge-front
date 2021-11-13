ece0c83a84aac4f9e563a95febd6809b9f72cc0a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-13 10:54:50 -0300

    feat(component): create new component
    create base button component

---
+++ src/components/AButton.vue
@@ -0,0 +1,75 @@
+<template>
+  <button 
+    class="AButton"
+    :class="getClasses"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AButton",
+  props: {
+    color: {
+      type: String,
+      validator: function(value) {
+        return ['primary', 'secondary'].indexOf(value) !== -1
+      }
+    },
+    text: {
+      type: String
+    },
+    small: {
+      type: Boolean
+    },
+    xSmall: {
+      type: Boolean
+    },
+    big: {
+      type: Boolean
+    }
+  },
+  computed: {
+    getClasses(){
+      return [
+        {
+          'AButton--xSmall': this.xSmall,
+          'AButton--small': this.small,
+          'AButton--big': this.big,
+          'AButton--default': !this.xSmall && !this.small && !this.big
+        },
+        'AButton--' + this.color
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.AButton {
+  background-color: white;
+  border: none;
+  font-family: Montserrat;
+  font-size: 1rem;
+  font-weight: normal;
+  border-radius: 2rem;
+  &:hover {
+    cursor: pointer;
+  };
+  &:active {
+    opacity: 0.8;
+  }
+}
+.AButton--default {
+  padding: 0.6rem 2.5rem;
+}
+.AButton--primary {
+  background-color: #00308F;
+  color: #fafafa;
+}
+.AButton--secondary {
+  background-color: firebrick;
+  color: #fafafa
+}
+</style>
