8a7c7ecf5f0d9af92a9ba69abde09a52e9167d6a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-20 10:07:40 -0300

    feat(component): create login view

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="Login">
+    <t-login />
+  </div>
+</template>
+
+<script>
+import TLogin from "@/components/TLogin";
+export default {
+  name: "Login",
+  components: {
+    TLogin
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.Login {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: calc(100vh - 1rem);
+}
+</style>
