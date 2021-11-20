96cae77c3dc19c3b5d0a8180bc995e14fbd3a3fe
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-20 10:48:27 -0300

    feat(component): create template to home page

---
+++ src/components/THome.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="THome">
+    <h1>Olá {{ user }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "THome",
+  props: {
+    user: {
+      type: String,
+      default() { return "Fulano" }
+    } 
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.THome {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+</style>
