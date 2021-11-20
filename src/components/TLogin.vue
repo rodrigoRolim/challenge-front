c72d634e33a000b1e423c51c01b101eafe3724ee
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-20 09:50:35 -0300

    feat(component): create template to login page
    the template represent the scaffold of the page

---
+++ src/components/TLogin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="TLogin">
+    <o-login-card />
+  </div>
+</template>
+
+<script>
+import OLoginCard from "./OLoginCard";
+export default {
+  name: "TLogin",
+  components: {
+    OLoginCard
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.TLogin {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2rem;
+}
+</style>
