977be9891443f6058b8feac1d1f513353dd8df77
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-20 10:55:52 -0300

    refactor(component): remove code
    remove all staff bind to redirect to login and to signup

--- src/components/MNavbar.vue
+++ src/components/MNavbar.vue
@@ -1,37 +1,12 @@
 <template>
   <nav class="MNavbar">
-    <a-button v-if="isSignupPage" text="login" @click="redirectToLogin" color="primary" outlined/>
-    <a-button v-if="isLoginPage" text="cadastre-se" color="success" @click="redirectToSignup" class="MNavbar__ButtonSignup"/>
+    
   </nav>
 </template>
 
 <script>
-import AButton from "./AButton";
-
 export default {
-  name: "MNavbar",
-  components: {
-    AButton
-  },
-  computed: {
-    isSignupPage() {
-      return this.$route.name === "Signup";
-    },
-    isLoginPage() {
-      return this.$route.name === "Login";
-    }
-  },
-  methods: {
-    redirectToLogin() {
-      this.$router.push("/login")
-    },
-    redirectToSignup() {
-      this.$router.push("/cadastro")
-    }
-  },
-  created() {
-    console.log(this.$route)
-  }
+  name: "MNavbar"
 }
 </script>
 
