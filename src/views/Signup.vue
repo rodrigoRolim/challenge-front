526d9cd8cb2be9ed1d69c7a7dd69e52059812f87
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-20 10:09:40 -0300

    feat(component): create signup view

---
+++ src/views/Signup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="Login">
+    <t-signup />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TSignup from "@/components/TSignup";
+export default {
+  name: 'Home',
+  components: {
+    TSignup
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.Login {
+  padding: 4rem 1rem 1rem 0rem;
+  min-height: calc(100vh - 1rem);
+}
+</style>
