039d3572eaf5b53441e7018536e4bde852ebcd92
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-20 10:57:31 -0300

    feat(component): create component to make redirections to login and signup

---
+++ src/components/MNavUnlogged.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="MNavUnlogged">
+    <a-button 
+      color="primary" 
+      outlined
+      text="login" 
+      v-if="isSignupPage" 
+      
+      @click="redirectToLogin" 
+    />
+    <a-button 
+      class="MNavbar__ButtonSignup"
+      color="success" 
+      text="cadastre-se" 
+      v-if="isLoginPage"
+
+      @click="redirectToSignup" 
+    />
+  </div>
+</template>
+
+<script>
+import AButton from "./AButton";
+
+export default {
+  name: "MNavUnlogged",
+  components: {
+    AButton
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
