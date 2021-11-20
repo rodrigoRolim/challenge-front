c799d3c7220eefc3c1da565ccfc2c208cfb7a032
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-20 13:41:21 -0300

    feat(component): create profile template

---
+++ src/components/TProfile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="TProfile">
+    <o-user-datas-card />
+    <o-user-address-card />
+    <o-user-password-card />
+  </div>
+</template>
+
+<script>
+import OUserDatasCard from "./OUserDatasCard";
+import OUserAddressCard from "./OUserAddressCard";
+import OUserPasswordCard from "./OUserPasswordCard";
+export default {
+  name: "TProfile",
+  components: {
+    OUserAddressCard,
+    OUserDatasCard,
+    OUserPasswordCard
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
