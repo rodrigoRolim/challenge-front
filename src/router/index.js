bba4e0191186049a36fddc93d47534d82961b85b
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-20 10:09:26 -0300

    feat(router): create routes to login and signup

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "Login" */ '../views/Login.vue')
+  },
+  {
+    path: '/cadastro',
+    name: 'Signup',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
   },
 ]
 
