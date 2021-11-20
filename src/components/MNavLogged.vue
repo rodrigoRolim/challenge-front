1bbb1429db74cc90eb676ac7e57bd51c0aedd02c
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-20 13:35:31 -0300

    feat(component): crete navigation when user is not logged

---
+++ src/components/MNavLogged.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="MNavLogged">
+    <a-button text="sair" color="secondary" outlined @click="logout"/>
+  </div>
+</template>
+
+<script>
+import AButton from './AButton.vue'
+export default {
+  name: "MNavLogged",
+  components: { AButton },
+  methods: {
+    logout() {
+      this.$router.push("/login")
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
