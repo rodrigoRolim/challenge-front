56f7c7a082e846e27bea94e1f59539dd0337eb2c
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-20 10:48:13 -0300

    feat(component): create the home page

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="Home">
+   <t-home :user="user" />
+  </div>
+</template>
+
+<script>
+import THome from "@/components/THome";
+export default {
+  name: "Home",
+  data() {
+    return {
+      user: "Rodrigo"
+    }
+  },
+  components: {
+    THome
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
