67ace6d9d4576e64ebda271a0eaca03de996fda0
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-26 09:33:06 -0300

    feat(component): remove alignment of text and change background color

--- src/App.vue
+++ src/App.vue
@@ -26,9 +26,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  background-color: #fafafa;
+  background-color: #f5f5f5;
   overflow: auto;
 }
 
