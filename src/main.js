6773eed0236ece074ee681b2c7926e7a12044089
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-29 16:16:54 -0300

    chore: add font awesome icons

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser, faEnvelope, faBook, faIdCard, faPencilAlt, faMapMarkedAlt, faCity } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(faUser, faEnvelope, faBook, faIdCard, faPencilAlt, faMapMarkedAlt, faCity);
+
+createApp(App).use(store).use(router).component('AIcon', FontAwesomeIcon).mount('#app')
