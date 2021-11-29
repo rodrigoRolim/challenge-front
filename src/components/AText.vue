3a0a31b0db833e377bc8490adc5378fa0e0b812d
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-29 16:18:14 -0300

    feat(component): create a new atomic component
    this component is used to be a text elements on html template

---
+++ src/components/AText.vue
@@ -0,0 +1,22 @@
+<template>
+  <component class="AText" :is="tag">{{ text }}</component>
+</template>
+
+<script>
+export default {
+  name: "AText",
+  props: {
+    tag: {
+      type: String,
+      default: "p"
+    },
+    text: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
