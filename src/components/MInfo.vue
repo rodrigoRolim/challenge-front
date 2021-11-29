8af10e6a0161ec48046c9362b64457557418340b
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-29 16:19:39 -0300

    feat(component): create a new molecule component
    this component is used to be a information that contains a icon following by text

---
+++ src/components/MInfo.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="MInfo">
+    <a-icon class="MInfo__Icon" :class="getColors" :icon="icon" :color="colorIcon" />
+    <a-text class="MInfo__Text" tag="span" :text="text" :color="colorText" />
+  </div>
+</template>
+
+<script>
+
+import AText from "./AText";
+
+export default {
+  name: "MInfo",
+  props: {
+    colorIcon: {
+      type: String,
+      validator: function(value) {
+        return ['primary', 'secondary', 'success', 'default'].indexOf(value) !== -1
+      },
+      default: 'default'
+    },
+    colorText: {
+      type: String,
+      validator: function(value) {
+        return ['primary', 'secondary', 'success'].indexOf(value) !== -1
+      }
+    },
+    icon: {
+      type: [Array, String],
+      required: true
+    },
+    text: {
+      type: String,
+      required: true,
+      default: () => ""
+    }
+  },
+  computed: {
+    getColors() {
+      return 'MInfo__Icon--' + this.colorIcon;
+    }
+  },
+  components: {
+    AText
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.MInfo {
+  display: flex;
+}
+.MInfo__Icon {
+  min-width: 30px;
+}
+.MInfo__Text {
+  margin-left: 1rem
+}
+.MInfo__Icon--default {
+  color: gray;
+}
+.MInfo__Icon--primary {
+  color: #00308F;
+}
+.MInfo__Icon--secondary {
+  color: firebrick;
+}
+.MInfo__Icon--success {
+  color: #5fc95b;
+}
+</style>
