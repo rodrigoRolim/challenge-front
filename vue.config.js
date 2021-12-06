e552bea08457126cff373c3e03ed426352c3cf07
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-12-06 17:03:20 -0300

    feat: add mixin to config responveness

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,12 @@ module.exports = {
     loaderOptions: {
       scss:{
         prependData: () => {
+
           let __fonts = `@import "@/styles/__fonts.scss";`;
           let __design = `@import "@/styles/__design.scss";`;
-          return __fonts + __design;
+          let __responsiveness = `@import "@/styles/__responsiveness";`;
+
+          return __fonts + __design + __responsiveness;
         }
       }
     }
