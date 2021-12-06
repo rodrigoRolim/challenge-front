24d8a74a6b1729ddf9c8397e070260c6f47bfef1
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-12-06 09:02:17 -0300

    feat(component): create component a editer of personal datas

---
+++ src/components/MInputsUser.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="MInputsUser">
+    <a-input 
+      label="nome" 
+      class="MInputsUser__Input  MarginB-2"
+      v-model="name"
+    />
+    <a-input label="e-mail" class="MInputsUser__Input  MarginB-2" />
+    <a-input label="cpf" class="MInputsUser__Input  MarginB-2" />
+    <a-input label="pis" class="MInputsUser__Input  MarginB-2" />
+  </div>
+</template>
+
+<script>
+import AInput from "./AInput";
+
+export default {
+  name: "MInputsUser",
+  components: {
+    AInput
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.MInputsUser {
+  width: 100%;
+}
+</style>
