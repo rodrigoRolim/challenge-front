c1865e9e3ca216cff97e98625bc5472665ff5e53
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-12-05 22:27:14 -0300

    feat(component): create component to be a link

---
+++ src/components/ALink.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="ALink">
+    <router-link :to="path">{{ name }}</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ALink",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    path: {
+      type: String,
+      default() { return "#" }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+a {
+  font-weight: 450;
+  color: #535d66;
+  text-decoration: none;
+  font-family: Montserrat;
+}
+</style>
