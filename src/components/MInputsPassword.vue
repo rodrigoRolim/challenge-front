1962a53f4b691af6f39afce4d9fba1ca71b91e06
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-12-06 08:58:42 -0300

    feat(component): create components to be a password editer

---
+++ src/components/MInputsPassword.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="MInputsPassword">
+    <a-input label="senha" class="MInputsPassword__Input  MarginB-2" />
+    <a-input label="confirmar senha" class="MInputsPassword__Input  MarginB-2" />
+  </div>
+</template>
+
+<script>
+import AInput from "./AInput";
+export default {
+  name: "MInputsPassword",
+  components: {
+    AInput
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/OUserPassword.vue
+++ src/components/OUserPassword.vue
@@ -1,20 +1,40 @@
 <template>
-  <div class="OUserPasswordCard">
-
+  <div class="OUserPassword">
+    <m-info icon="key" text="szpvnp77" />
+    <a-button 
+      class="OUserPassword__Button" 
+      text="mudar senha" 
+      color="brand" 
+      x-small
+      @click="updatePassword"
+    />
   </div>
 </template>
 
 <script>
+import AButton from "./AButton";
+import MInfo from "./MInfo";
+
 export default {
   name: "OUserPassword",
   components: {
-
+    AButton,
+    MInfo
+  },
+  methods: {
+    updatePassword() {
+      this.$emit('open-editer-password');
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.OUserPasswordCard {
-  padding: 0rem 1rem;
+.OUserPassword {
+  display: flex;
+  align-items: center;
+}
+.OUserPassword__Button {
+  margin-left: 1rem;
 }
 </style>
