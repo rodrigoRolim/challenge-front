8dba3665aed086980c4eb4814135c1cdb6ebf077
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-12-05 22:28:11 -0300

    feat(component): create component to be a group o inputs about user address

---
+++ src/components/MInputsAddress.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="MInputsAddress">
+    <a-input 
+      label="cep" 
+      class="MInputsAddress__Input MInputsAddress__MinorInput  MarginB-2" 
+    />
+    <div class="MInputsAddress__GroupInput">
+      <a-select 
+        label="país" 
+        class="MInputsAddress__Input MInputsAddress__MinorInput  MarginB-2" 
+      />
+      <a-select 
+        label="estado" 
+        class="MInputsAddress__Input MInputsAddress__MinorInput  MarginB-2" 
+      />
+    </div>
+    <a-input 
+      label="rua" 
+      class="MInputsAddress__Input  MarginB-2" 
+    />
+    <div class="MInputsAddress__GroupInput">
+      <a-input 
+        label="número" 
+        class="MInputsAddress__Input MInputsAddress__MinorInput  MarginB-2" 
+      />
+      <a-input 
+        label="complemento" 
+        class="MInputsAddress__Input MInputsAddress__MinorInput  MarginB-2" 
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import AInput from "./AInput";
+import ASelect from "./ASelect";
+
+export default {
+  name: "MInputsAddress",
+  components: {
+    AInput,
+    ASelect
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.MInputsAddress__Input {
+  width: 100%;
+}
+.MInputsAddress__GroupInput {
+  display: flex;
+  justify-content: space-between;
+}
+.MInputsAddress__MinorInput {
+  max-width: 290px;
+}
+</style>
