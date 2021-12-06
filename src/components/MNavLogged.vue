c8bb8a3d2340d9206c519ed5d2e350d05587b33f
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-12-06 09:07:14 -0300

    feat(component): add link to navbar
    link: user perfil

--- src/components/MNavLogged.vue
+++ src/components/MNavLogged.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="MNavLogged">
+    <a-link class="MNavLogged__Link" name="perfil" path="/dados"/>
     <a-button text="sair" color="secondary" outlined @click="logout"/>
   </div>
 </template>
 
 <script>
-import AButton from './AButton.vue'
+import AButton from "./AButton.vue";
+import ALink from "./ALink";
+
 export default {
   name: "MNavLogged",
-  components: { AButton },
+  components: { 
+    AButton,
+    ALink
+  },
   methods: {
     logout() {
       this.$router.push("/login")
@@ -18,5 +24,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.MNavLogged {
+  display: flex;
+  align-items: center;
+}
+.MNavLogged__Link {
+  margin-right: 1rem;
+}
 </style>
