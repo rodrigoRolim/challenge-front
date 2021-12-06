df6a939b9209f05c96e44bfe7ac4650d98bb07eb
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-12-06 09:26:54 -0300

    feat(component): open modal editer when user click on pencil icon

--- src/components/TProfile.vue
+++ src/components/TProfile.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="TProfile">
-    <o-profile />
+    <o-profile @open-editer="openEditer"/>
+    <o-profile-edit :open-modal="openModal" :edit-to="beingUpdated" @close-modal="openModal = $event"/>
   </div>
 </template>
 
 <script>
 import OProfile from "./OProfile";
+import OProfileEdit from "./OProfileEdit";
 
 export default {
   name: "TProfile",
+  data() {
+    return {
+      beingUpdated: "",
+      openModal: false
+    }
+  },
   components: {
-    OProfile
+    OProfile,
+    OProfileEdit
+  },
+  methods: {
+    openEditer(beingUpdated) {
+      this.beingUpdated = beingUpdated;
+      this.openModal = true;
+    }
   }
 }
 </script>
