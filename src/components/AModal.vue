1bdce49bfe9778a55ab81eda82e2663343fce1fd
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-12-05 22:27:32 -0300

    feat(component): create component to be a modal

---
+++ src/components/AModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="AModal" v-if="open">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AModal",
+  props: {
+    open: {
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.AModal {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
